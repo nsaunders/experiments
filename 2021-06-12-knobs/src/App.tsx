14d40a4b760696b70c4ae84c3d4df9b53e19e86e
Nick Saunders <nick@saunde.rs>
2021-06-14 05:59:28 -0700

    knobs refactoring

--- 2021-06-12-knobs/src/App.tsx
+++ 2021-06-12-knobs/src/App.tsx
@@ -1,3 +1,5 @@
+import * as F from "./Flare";
+import { Flare } from "./Flare";
 import { pipe } from "fp-ts/lib/function";
 import React, { FC, ReactNode, useEffect, useState } from "react";
 import { Button } from "./Button";
@@ -8,76 +10,7 @@ interface UI<A> {
   render: (onChange: () => void) => [Node[], () => void];
 }
 
-function number({ defaultValue }: { defaultValue: number; }): UI<number> {
-  let input: HTMLInputElement | null = null;
-  return {
-    initialState: defaultValue,
-    query: () => {
-      const value = input ? parseFloat(input.value) : NaN;
-      return isNaN(value) ? defaultValue : value;
-    },
-    render: onChange => {
-      input = document.createElement("input");
-      input.type = "number";
-      input.value = defaultValue.toString();
-      input.addEventListener("input", onChange);
-      return [
-        [input],
-        () => { input && input.removeEventListener("input", onChange); },
-      ];
-    },
-  };
-}
-
-function string({ defaultValue }: { defaultValue: string; }): UI<string> {
-  let input: HTMLInputElement | null = null;
-  return {
-    initialState: defaultValue,
-    query: () => input ? input.value : defaultValue,
-    render: onChange => {
-      input = document.createElement("input");
-      input.value = defaultValue.toString();
-      input.addEventListener("input", onChange);
-      return [
-        [input],
-        () => { input && input.removeEventListener("input", onChange); },
-      ];
-    },
-  };
-}
-
-function apUI<A, B>(fa: UI<A>) {
-  return (fatob: UI<(a: A) => B>): UI<B> => ({
-    initialState: fatob.initialState(fa.initialState),
-    query: () => fatob.query()(fa.query()),
-    render: onChange => {
-      const [nodesA, teardownA] = fatob.render(onChange);
-      const [nodesB, teardownB] = fa.render(onChange);
-      return [
-        nodesA.concat(nodesB),
-        () => { teardownA(); teardownB(); }
-      ];
-    },
-  });
-}
-
-function pureUI<A>(a: A): UI<A> {
-  return {
-    initialState: a,
-    query: () => a,
-    render: () => [[], () => {}],
-  };
-}
-
-function mapUI<A, B>(fn: (a: A) => B) {
-  return (a: UI<A>): UI<B> => ({
-    initialState: fn(a.initialState),
-    query: () => fn(a.query()),
-    render: a.render,
-  });
-}
-
-function makeDemo<A>(knobs: UI<A>, render: (_: A) => ReactNode): FC<{}> {
+function makeDemo<A>(knobs: Flare<A>, render: (_: A) => ReactNode): FC<{}> {
   return function() {
     const [state, setState] = useState<A>(knobs.initialState);
     const onChange = () => { setState(knobs.query()); };
@@ -106,21 +39,15 @@ function makeDemo<A>(knobs: UI<A>, render: (_: A) => ReactNode): FC<{}> {
   };
 }
 
-const aUI = {
-  map: mapUI,
-  of: pureUI,
-  ap: apUI,
-};
-
 export const App = makeDemo(
   pipe(
-    aUI.of((name: string) => (age: number) => `${name} is ${age}`),
-    aUI.ap(string({ defaultValue: "Nicolin" })),
-    aUI.ap(
+    F.of((name: string) => (age: number) => (exclaim: boolean) => `${name} is ${age}${exclaim ? "!" : "."}`),
+    F.ap(F.string({ defaultValue: "Nicolin" })),
+    F.ap(
       pipe(
-        aUI.of((a: number) => (b: number) => a + b),
-        aUI.ap(number({ defaultValue: 5 })),
-        aUI.ap(number({ defaultValue: 8 })),
+        F.of((a: number) => (b: number) => a + b),
+        F.ap(F.number({ defaultValue: 5 })),
+        F.ap(F.number({ defaultValue: 8 })),
       ),
     ),
   ),
---
+++ 2021-06-12-knobs/src/Flare.ts
@@ -0,0 +1,124 @@
+import { Monad1 } from "fp-ts/Monad";
+
+export const URI = "Flare";
+
+export type URI = typeof URI;
+
+declare module "fp-ts/lib/HKT" {
+  interface URItoKind<A> {
+    readonly Flare: Flare<A>
+  }
+}
+
+export interface Flare<A> {
+  initialState: A;
+  query: () => A;
+  render: (onChange: () => void) => [Node[], () => void];
+}
+
+export const flare: Monad1<URI> = {
+  URI,
+  ap: function(fab, fa) {
+    return {
+      initialState: fab.initialState(fa.initialState),
+      query: () => fab.query()(fa.query()),
+      render: onChange => {
+        const [nodesA, teardownA] = fab.render(onChange);
+        const [nodesB, teardownB] = fa.render(onChange);
+        return [
+          nodesA.concat(nodesB),
+          () => { teardownA(); teardownB(); }
+        ];
+      },
+    };
+  },
+  chain: function(fa, f) {
+    return {
+      ...f(fa.query()),
+      initialState: f(fa.initialState).initialState,
+    };
+  },
+  map: function(ma, f) {
+    return {
+      initialState: f(ma.initialState),
+      query: () => f(ma.query()),
+      render: ma.render,
+    };
+  },
+  of: function(a) {
+    return {
+      initialState: a,
+      query: () => a,
+      render: () => [[], () => {}],
+    };
+  },
+};
+
+function curry2<A, B, Y>(f: (a: A, b: B) => Y): (a: A) => (b: B) => Y {
+  return a => b => f(a, b);
+}
+
+function flip2<A, B, Y>(f: (a: A, b: B) => Y): (b: B, a: A) => Y {
+  return (a, b) => f(b, a);
+}
+
+export const ap = curry2(flip2(flare.ap));
+export const chain = curry2(flare.chain);
+export const map = curry2(flare.map);
+export const of = flare.of;
+
+export function checkbox({ defaultValue }: { defaultValue: boolean; }): Flare<boolean> {
+  let input: HTMLInputElement | null = null;
+  return {
+    initialState: defaultValue,
+    query: () => input ? input.checked : defaultValue,
+    render: onChange => {
+      input = document.createElement("input");
+      input.type = "checkbox";
+      input.checked = defaultValue;
+      input.addEventListener("change", onChange);
+      return [
+        [input],
+        () => { input && input.removeEventListener("change", onChange); },
+      ];
+    },
+  };
+}
+
+export function number({ defaultValue }: { defaultValue: number; }): Flare<number> {
+  let input: HTMLInputElement | null = null;
+  return {
+    initialState: defaultValue,
+    query: () => {
+      const value = input ? parseFloat(input.value) : NaN;
+      return isNaN(value) ? defaultValue : value;
+    },
+    render: onChange => {
+      input = document.createElement("input");
+      input.type = "number";
+      input.value = defaultValue.toString();
+      input.addEventListener("input", onChange);
+      return [
+        [input],
+        () => { input && input.removeEventListener("input", onChange); },
+      ];
+    },
+  };
+}
+
+export function string({ defaultValue }: { defaultValue: string; }): Flare<string> {
+  let input: HTMLInputElement | null = null;
+  return {
+    initialState: defaultValue,
+    query: () => input ? input.value : defaultValue,
+    render: onChange => {
+      input = document.createElement("input");
+      input.value = defaultValue.toString();
+      input.addEventListener("input", onChange);
+      return [
+        [input],
+        () => { input && input.removeEventListener("input", onChange); },
+      ];
+    },
+  };
+}
